1b4274f50f00e76cbfe7f1c326d0a1e73d126891
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-01-05 11:48:42 +0700

    Update UI

---
+++ app/src/main/java/com/example/healthcare_mobile/di/HealthBooking_MobileApp.kt
@@ -0,0 +1,8 @@
+package com.example.healthcare_mobile.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HealthBooking_MobileApp: Application() {
+}
--- app/src/main/java/com/example/healthcare_mobile/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/healthcare_mobile/ui/home/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.healthcare_mobile.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.healthcare_mobile.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
 }
