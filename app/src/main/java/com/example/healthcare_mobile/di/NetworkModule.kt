9c8ebd5a24f38a2e2aa3d55dfbae17d1d887107b
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-01-05 11:48:54 +0700

    Update UI

---
+++ app/src/main/java/com/example/healthcare_mobile/di/NetworkModule.kt
@@ -0,0 +1,19 @@
+package com.example.healthcarecomp.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+}
